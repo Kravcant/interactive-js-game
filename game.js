97cbb5c9587e009a149909fc5bf8dd8f40b7bc18
anthonykravchishin <tonykravc53@gmail.com>
2026-04-07 11:04:53 -0700

    Added enemy HP to game.js

--- game.js
+++ game.js
@@ -1,16 +1,23 @@
 let score = 0;
-let pointsPerClick = 1;
+let damagePerClick = 1;
+let nextEnemyHP = 10;
+let enemyHP = nextEnemyHP;
 
 
 //function to update the score when the button is clicked
 function updateDisplay(){
-   document.getElementById("score=display").textContent = score;
-   document.getElementById("points-per-click-display").textContent = pointsPerClick;
+   document.getElementById("score-display").textContent = `Score: ${score}`;
+   document.getElementById("rate-display").textContent = `Damage per Click: ${damagePerClick}`;
+   document.getElementById("health-display").textContent = `HP: ${enemyHP}`;
 }
 
 
 //event listener for the click button
-document.getElementById("click-button").addEventListener("click", function(){{
-    score += pointsPerClick;
+document.getElementById("click-btn").addEventListener("click", function(){{
+    enemyHP -= damagePerClick;
+    if (enemyHP <= 0) {
+        score += Math.round(nextEnemyHP / 10);
+        enemyHP = nextEnemyHP;
+    }
     updateDisplay();
 }})
