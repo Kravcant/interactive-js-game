732708a8e933105ac7825878732117380ec79481
anthonykravchishin <tonykravc53@gmail.com>
2026-04-07 11:17:36 -0700

    Added levels

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 let score = 0;
 let damagePerClick = 1;
-let nextEnemyHP = 10;
+let enemiesKilled = 0;
+let level = 1;
+let nextEnemyHP = 10 * level;
 let enemyHP = nextEnemyHP;
 
 
@@ -17,7 +19,13 @@ document.getElementById("click-btn").addEventListener("click", function(){{
     enemyHP -= damagePerClick;
     if (enemyHP <= 0) {
         score += Math.round(nextEnemyHP / 10);
+        enemiesKilled++;
+        if (enemiesKilled == 10) {
+            enemiesKilled = 0;
+            level++;
+            nextEnemyHP = 10 * level;
+        }
         enemyHP = nextEnemyHP;
     }
     updateDisplay();
-}})
+}});
